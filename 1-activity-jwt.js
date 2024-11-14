6ae8442ca662ea63b6d162689bcc482848b4e912
Khem <tempkhem@gmail.com>
2024-11-14 16:22:37 +0700

    feat(jwt-activities&postman)

---
+++ 1-activity-jwt.js
@@ -0,0 +1,26 @@
+// Import JWT dependency
+const jwt = require('jsonwebtoken');
+
+// Tips: User from database after the user logging in
+const user = { 
+  username: "khem",
+  role: "admin",
+  favorite: {
+    numbers: [3, 99]
+  }
+};
+
+// Tips: Should be store in the .env file
+const JWT_SECRET = "your-secret";
+
+// Generating a JWT Token
+const token = jwt.sign({ user }, JWT_SECRET, { expiresIn: '1h' });
+console.log('token = ', token);
+
+// Verifying a JWT Token
+jwt.verify(token, JWT_SECRET, function(err, decodedToken) {
+  if (err) {
+    console.log('err = ', err);
+  }
+  console.log('decodedToken = ', decodedToken);
+});
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const authRoutes = require('./api/v1/routes/auth');
-const authenticateToken = require('./api/v1/routes/authenticateToken');
-
+const authenticateToken = require('./middlewares/authenticateToken');
 
 const app = express();
 app.use(express.json()); // Use express.json() to parse JSON request bodies
 
 app.use('/api/v1/auth', authRoutes);
 
-// Example protected route
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "This is a normal route" });
+});
+
 app.get('/protected', authenticateToken, (req, res) => {
     res.status(200).json({ message: "This is a protected route", user: req.user });
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
